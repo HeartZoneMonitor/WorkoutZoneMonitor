7fc28e8f937416f9f3d4ec5288c01ed85cde9774
rndas27 <rohan24@illinois.edu>
2025-02-15 14:33:06 -0600

    Added heart rate monitor (doesn't work)

--- Hackathon/src/main.cpp
+++ Hackathon/src/main.cpp
@@ -1,8 +1,13 @@
 #include <Arduino.h>
 #include "SparkFun_MMA8452Q.h"
+#include "PulseSensorPlayground.h"
 #include "Wire.h"
 
-MMA8452Q accel(0x1D); // Default MMA8452Q object create. (Address
+MMA8452Q accel(0x1D); // Default MMA8452Q object create.
+
+PulseSensorPlayground pulseSensor;
+
+int Threshold = 470; 
 
 
 void setup() {
@@ -10,6 +15,9 @@ void setup() {
   Serial.begin(9600);
 
   accel.init(); // Default init: +/-2g and 800Hz ODR
+  pulseSensor.analogInput(0);
+  pulseSensor.setThreshold(Threshold);
+  pulseSensor.begin();
 
 
   
@@ -17,20 +25,26 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  accel.read(); // Update acceleromter data
+  // accel.read(); // Update acceleromter data
 
-  Serial.print("Acceleration on the x-axis is: ");
-  Serial.print(accel.cx);
-  Serial.println(" g's");
+  // Serial.print("Acceleration on the x-axis is: ");
+  // Serial.print(accel.cx);
+  // Serial.println(" g's");
 
-  Serial.print("Acceleration on the y-axis is: ");
-  Serial.print(accel.cy);
-  Serial.println(" g's");
+  // Serial.print("Acceleration on the y-axis is: ");
+  // Serial.print(accel.cy);
+  // Serial.println(" g's");
 
-  Serial.print("Acceleration on the z-axis is: ");
-  Serial.print(accel.cz);
-  Serial.println(" g's");
-  delay(1000);
+  // Serial.print("Acceleration on the z-axis is: ");
+  // Serial.print(accel.cz);
+  // Serial.println(" g's");
+
+  if(pulseSensor.sawStartOfBeat()) {
+    int myBPM = pulseSensor.getBeatsPerMinute();
+    Serial.println("Heartrate:" );
+    Serial.println(myBPM);
+  }
+  //delay(2000);
 
 
 }
